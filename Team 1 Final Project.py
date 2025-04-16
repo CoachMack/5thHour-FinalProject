3d81467033a5c78a1942f0abc7c74dbd9555acb8
Zachary Martin <zm125045@student.wpsok.org>
2025-04-16 13:00:05 -0500

    Final project

---
+++ Team 1 Final Project.py
@@ -0,0 +1,6 @@
+#Names: Zachary Martin, Kevin Zheng, Preston Bidwell, Ethan Wright, Sam Raymond
+#Team: 1
+#Class: 5th Hour
+#Assignment: Final Project
+
+#Scrum Template: https://docs.google.com/document/d/1lj8xZ0Y5f0yIAYmHzDW40DiCe2NXuIek-VHHGVOzF1o/edit?usp=sharing
